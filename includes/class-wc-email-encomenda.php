224b3bffb66f50b3321eed5237218794f0fb1742
faelmarcondeli <64538814+faelmarcondeli@users.noreply.github.com>
2025-07-18 14:09:38 -0300

    Update class-wc-email-encomenda.php

--- includes/class-wc-email-encomenda.php
+++ includes/class-wc-email-encomenda.php
@@ -10,12 +10,30 @@ class WC_Email_Encomenda extends WC_Email {
         $this->subject        = __( '[{site_title}] Encomenda do seu Pedido #{order_number}', 'wc-backorder-confirmation' );
         $this->heading        = __( 'Detalhes da sua Encomenda', 'wc-backorder-confirmation' );
 
+        $this->placeholders = [
+            '{order_number}' => '',
+        ];
+
         $this->template_html  = 'emails/encomenda.php';
         $this->template_plain = 'emails/plain/encomenda.php';
 
         $this->customer_email = true;
 
         parent::__construct();
+
+        add_filter( 'woocommerce_email_enabled_customer_processing_order', [ $this, 'maybe_disable_default_processing_email' ], 10, 3 );
+    }
+
+    public function init_form_fields() {
+        parent::init_form_fields();
+
+        $this->form_fields['disable_regular_processing_email'] = [
+            'title'       => __( 'Apenas este email', 'wc-backorder-confirmation' ),
+            'type'        => 'checkbox',
+            'label'       => __( 'Não enviar o email de "Pedido em Processamento" padrão quando este email for enviado', 'wc-backorder-confirmation' ),
+            'default'     => 'no',
+            'description' => __( 'Quando marcado, substitui o email de pedido em processamento padrão para pedidos com itens sob encomenda.', 'wc-backorder-confirmation' ),
+        ];
     }
 
     public function trigger( $order_id, $order = false ) {
@@ -28,6 +46,8 @@ class WC_Email_Encomenda extends WC_Email {
 
         $this->object    = $order;
         $this->recipient = $order->get_billing_email();
+
+        $this->placeholders['{order_number}'] = $order->get_order_number();
 
         if ( ! $this->is_enabled() || ! $this->get_recipient() ) {
             return;
@@ -70,5 +90,15 @@ class WC_Email_Encomenda extends WC_Email {
             '',
             plugin_dir_path( __FILE__ ) . '../templates/'
         );
+    }
+
+    public function maybe_disable_default_processing_email( $enabled, $order, $email ) {
+        if ( 'yes' === $this->get_option( 'disable_regular_processing_email', 'no' ) &&
+             $order instanceof WC_Order &&
+             'yes' === $order->get_meta( 'has_sob_encomenda' ) ) {
+            return false;
+        }
+
+        return $enabled;
     }
 }
